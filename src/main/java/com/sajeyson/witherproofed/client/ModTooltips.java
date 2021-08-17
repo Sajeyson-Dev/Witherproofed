86167b9c9d3d44c7671ff99a51c91cc810649ac1
Sajeyson <42738314+Sajeyson@users.noreply.github.com>
2021-08-17 15:13:37 +0300

    Meh...

--- src/main/java/com/sajeyson/witherproofed/client/ModTooltips.java
+++ src/main/java/com/sajeyson/witherproofed/client/ModTooltips.java
@@ -5,9 +5,8 @@ import net.minecraft.network.chat.TextComponent;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
+@OnlyIn(Dist.CLIENT)
 public class ModTooltips {
-
-    @OnlyIn(Dist.CLIENT)
     public static final TextComponent DEFAULT_BLOCK_TOOLTIP = (TextComponent)
             new TextComponent("Wither cannot destroy this block").withStyle(ChatFormatting.AQUA);
 }

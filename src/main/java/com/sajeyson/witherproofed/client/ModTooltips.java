6315841c4356db4ca6784041ea416fd30a177c95
Sajeyson <42738314+Sajeyson@users.noreply.github.com>
2021-08-17 10:07:36 +0300

    Added CTM tooltip.

--- src/main/java/com/sajeyson/witherproofed/client/ModTooltips.java
+++ src/main/java/com/sajeyson/witherproofed/client/ModTooltips.java
@@ -5,9 +5,12 @@ import net.minecraft.util.text.TextFormatting;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
+@OnlyIn(Dist.CLIENT)
 public class ModTooltips {
 
-    @OnlyIn(Dist.CLIENT)
     public static final StringTextComponent DEFAULT_BLOCK_TOOLTIP = (StringTextComponent)
             new StringTextComponent("Wither cannot destroy this block").withStyle(TextFormatting.AQUA);
+
+    public static final StringTextComponent HAS_CTM_TOOLTIP = (StringTextComponent)
+            new StringTextComponent("Has connected texture").withStyle(TextFormatting.GRAY);
 }

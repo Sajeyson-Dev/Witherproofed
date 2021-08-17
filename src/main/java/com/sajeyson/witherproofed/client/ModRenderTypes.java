a6c652bf5a2f3a263cf8678520a38ae9e9a35bc1
Sajeyson <42738314+Sajeyson@users.noreply.github.com>
2021-08-17 10:05:22 +0300

    Refactor.

--- src/main/java/com/sajeyson/witherproofed/client/ModRenderTypes.java
+++ src/main/java/com/sajeyson/witherproofed/client/ModRenderTypes.java
@@ -6,10 +6,10 @@ import net.minecraft.client.renderer.RenderTypeLookup;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
-public class GlassBlocksRender {
+public class ModRenderTypes {
 
     @SubscribeEvent
-    public static void GlassRender(FMLClientSetupEvent event) {
+    public static void RenderTypes(FMLClientSetupEvent event) {
         RenderTypeLookup.setRenderLayer(ModBlocks.HEAVY_REINFORCED_GLASS.get(), RenderType.cutoutMipped());
         RenderTypeLookup.setRenderLayer(ModBlocks.HEAVY_REINFORCED_TRANSLUCENT_GLASS.get(), RenderType.translucent());
     }

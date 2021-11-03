5f73be2091cb6f4eb9e56dfe00c5fe9cf523d149
Sajeyson <42738314+Sajeyson@users.noreply.github.com>
2021-11-03 16:10:40 +0200

    Fixed crash on server.

--- src/main/java/com/sajeyson/witherproofed/client/ModRenderTypes.java
+++ src/main/java/com/sajeyson/witherproofed/client/ModRenderTypes.java
@@ -3,14 +3,19 @@ package com.sajeyson.witherproofed.client;
 import com.sajeyson.witherproofed.ModBlocks;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.RenderTypeLookup;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
 public class ModRenderTypes {
 
+    @OnlyIn(Dist.CLIENT)
     @SubscribeEvent
-    public static void RenderTypes(FMLClientSetupEvent event) {
+    public static void RenderTypes(final FMLClientSetupEvent event) {
         RenderTypeLookup.setRenderLayer(ModBlocks.HEAVY_REINFORCED_GLASS.get(), RenderType.cutoutMipped());
         RenderTypeLookup.setRenderLayer(ModBlocks.HEAVY_REINFORCED_TRANSLUCENT_GLASS.get(), RenderType.translucent());
+        RenderTypeLookup.setRenderLayer(ModBlocks.HEAVY_REINFORCED_GHOSTLY_GLASS.get(), RenderType.cutoutMipped());
+        RenderTypeLookup.setRenderLayer(ModBlocks.HEAVY_REINFORCED_GHOSTLY_TRANSLUCENT_GLASS.get(), RenderType.translucent());
     }
 }

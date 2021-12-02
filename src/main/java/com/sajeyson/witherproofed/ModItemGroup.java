ee7f9951c2c1faf60c58b7d255f19d45c4beab27
Sajeyson <42738314+Sajeyson@users.noreply.github.com>
2021-12-02 11:08:28 +0200

    Moved.

--- src/main/java/com/sajeyson/witherproofed/ModItemGroup.java
+++ src/main/java/com/sajeyson/witherproofed/ModItemGroup.java
@@ -1,12 +1,11 @@
-package com.sajeyson.witherproofed.client;
+package com.sajeyson.witherproofed;
 
-import com.sajeyson.witherproofed.ModItems;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 
 public class ModItemGroup {
 
-    public static final ItemGroup witherproofedItemGroup = new ItemGroup("witherproofedItemGroup") {
+    public static final ItemGroup MOD_ITEMGROUP = new ItemGroup("witherproofedItemGroup") {
         @Override
         public ItemStack makeIcon() {
             return new ItemStack(ModItems.GHOSTLY_ESSENCE.get());

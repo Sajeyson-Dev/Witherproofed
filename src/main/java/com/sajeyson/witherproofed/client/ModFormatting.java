973dc186e31888807cff75d4594029dd6a6f03b3
sajeyson012 <sajeysondude+@gmail.com>
2022-07-23 18:26:05 +0300

    Added custom formatting (font colors)

---
+++ src/main/java/com/sajeyson/witherproofed/client/ModFormatting.java
@@ -0,0 +1,15 @@
+package com.sajeyson.witherproofed.client;
+
+import net.minecraft.network.chat.Style;
+import net.minecraft.network.chat.TextColor;
+
+public class ModFormatting {
+
+    private static Style newColor(String color) {
+        return Style.EMPTY.withColor(TextColor.parseColor(color));
+    }
+
+    public static final Style SKU_BLUE = newColor("#00bfff");
+    public static final Style LIGHT_PURPLE = newColor("#9494b8");
+    public static final Style ACTUAL_GREEN = newColor("#00b36b");
+}

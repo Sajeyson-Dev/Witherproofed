d5621a359bb1bfa46b26652cef6dbc4875756bd7
sajeyson012 <sajeysondude+@gmail.com>
2022-07-23 18:30:48 +0300

    Update Witherproofed.java

--- src/main/java/com/sajeyson/witherproofed/Witherproofed.java
+++ src/main/java/com/sajeyson/witherproofed/Witherproofed.java
@@ -1,28 +1,29 @@
 package com.sajeyson.witherproofed;
 
+import org.slf4j.Logger;
+
+import com.mojang.logging.LogUtils;
 import com.sajeyson.witherproofed.client.ModRenderTypes;
+
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 @Mod(Witherproofed.MOD_ID)
 public class Witherproofed {
     public static final String MOD_ID = "witherproofed";
-    public static final Logger LOGGER = LogManager.getLogger();
+    public static final Logger LOGGER = LogUtils.getLogger();
 
     public Witherproofed() {
         MinecraftForge.EVENT_BUS.register(this);
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-        ModItems.ITEMS.register(modEventBus);
-        ModBlocks.BLOCKS.register(modEventBus);
+
+        ModRegistry.ITEMS.register(modEventBus);
+        ModRegistry.BLOCKS.register(modEventBus);
         FMLJavaModLoadingContext.get().getModEventBus().register(ModRenderTypes.class);
-        Witherproofed.LOGGER.info("So Heavy!");
-        if(ModList.get().isLoaded("ctm")) {
-            LOGGER.info("CTM Detected");
-        }
+
+        if(ModList.get().isLoaded("ctm")) LOGGER.info("CTM Detected!");
     }
 }

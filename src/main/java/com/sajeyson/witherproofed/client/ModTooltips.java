b1d6b36746a09ee559dbc7762a61c8aed2adf581
sajeyson012 <sajeysondude+@gmail.com>
2022-07-31 09:34:37 +0300

    Why this is boolean?

--- src/main/java/com/sajeyson/witherproofed/client/ModTooltips.java
+++ src/main/java/com/sajeyson/witherproofed/client/ModTooltips.java
@@ -11,32 +11,27 @@ import net.minecraftforge.api.distmarker.OnlyIn;
 
 @OnlyIn(Dist.CLIENT)
 public class ModTooltips {
-    public static boolean buildTooltip(List<Component> list, String tooltip, Style color) {
+    public static void buildTooltip(List<Component> list, String tooltip, Style color) {
         list.add(Component.translatable("tooltip.witherproofed." + tooltip).withStyle(color));
-        return false;
     }
 
-    public static boolean buildTooltip(List<Component> list, String tooltip, ChatFormatting color) {
+    public static void buildTooltip(List<Component> list, String tooltip, ChatFormatting color) {
         list.add(Component.translatable("tooltip.witherproofed." + tooltip).withStyle(color));
-        return false;
     }
 
-    public static boolean buildShiftTooltip(List<Component> list, String tooltip, Style color) {
+    public static void buildShiftTooltip(List<Component> list, String tooltip, Style color) {
         if (Screen.hasShiftDown()) list.add(Component.translatable("tooltip.witherproofed." + tooltip + ".shift").withStyle(color));
         else list.add(Component.translatable("tooltip.witherproofed.shift").withStyle(ChatFormatting.DARK_GRAY));
-        return false;
     }
 
-    public static boolean buildShiftTooltip(List<Component> list, String tooltip, int count, Style color) {
+    public static void buildShiftTooltip(List<Component> list, String tooltip, int count, Style color) {
         if (Screen.hasShiftDown()) for (int index = 0; index < count; index++) list.add(Component.translatable("tooltip.witherproofed." + tooltip + ".shift_" + index).withStyle(color));
         else list.add(Component.translatable("tooltip.witherproofed.shift").withStyle(ChatFormatting.DARK_GRAY));
-        return false;
     }
 
-    public static boolean buildSetBonusTooltip(List<Component> list, String tooltip, int count, Style color) {
+    public static void buildSetBonusTooltip(List<Component> list, String tooltip, int count, Style color) {
         list.add(Component.translatable("tooltip.witherproofed.fullset").withStyle(ModFormatting.ACTUAL_GREEN).withStyle(ChatFormatting.UNDERLINE));
         for (int index = 0; index < count; index++) list.add(Component.translatable("tooltip.witherproofed.fullset_" + tooltip + "_" + index).withStyle(color));
         list.add(Component.literal(""));
-        return false;
     }
 }

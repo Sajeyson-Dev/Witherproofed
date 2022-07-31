78fecb76039ea607ba0699e43953f02025f0d85f
sajeyson012 <sajeysondude+@gmail.com>
2022-07-31 09:46:39 +0300

    Funny

--- src/main/java/com/sajeyson/witherproofed/client/ModTooltips.java
+++ src/main/java/com/sajeyson/witherproofed/client/ModTooltips.java
@@ -14,25 +14,21 @@ import net.minecraftforge.api.distmarker.OnlyIn;
 
 @OnlyIn(Dist.CLIENT)
 public class ModTooltips {
-    public static boolean buildTooltip(List<Component> list, String tooltip, Style color) {
+    public static void buildTooltip(List<Component> list, String tooltip, Style color) {
         list.add(new TranslatableComponent("tooltip.witherproofed." + tooltip).withStyle(color));
-        return false;
     }
 
-    public static boolean buildTooltip(List<Component> list, String tooltip, ChatFormatting color) {
+    public static void buildTooltip(List<Component> list, String tooltip, ChatFormatting color) {
         list.add(new TranslatableComponent("tooltip.witherproofed." + tooltip).withStyle(color));
-        return false;
     }
 
-    public static boolean buildShiftTooltip(List<Component> list, String tooltip, Style color) {
+    public static void buildShiftTooltip(List<Component> list, String tooltip, Style color) {
         if (Screen.hasShiftDown()) list.add(new TranslatableComponent("tooltip.witherproofed." + tooltip + ".shift").withStyle(color));
         else list.add(new TranslatableComponent("tooltip.witherproofed.shift").withStyle(ChatFormatting.DARK_GRAY));
-        return false;
     }
 
-    public static boolean buildShiftTooltip(List<Component> list, String tooltip, int count, Style color) {
+    public static void buildShiftTooltip(List<Component> list, String tooltip, int count, Style color) {
         if (Screen.hasShiftDown()) for (int index = 0; index < count; index++) list.add(new TranslatableComponent("tooltip.witherproofed." + tooltip + ".shift_" + index).withStyle(color));
         else list.add(new TranslatableComponent("tooltip.witherproofed.shift").withStyle(ChatFormatting.DARK_GRAY));
-        return false;
     }
 }
